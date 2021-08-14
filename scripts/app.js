20e702466901a6d93901a4ed48124a5d6f16b005
eleftheros <eleftheros_all@protonmail.com>
2021-08-14 15:13:32 +0300

    Update updateCardsUI function

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,21 @@
-
-
+// function that takes an array of characters and update each one with a card in the DOM
 const updateCardsUI = characters => {
   const cardsList = document.getElementById("cardsList");
+  cardsList.innerHTML = "";
   const fragment = new DocumentFragment();
 
   characters.forEach(character => {
-    // create card
+    // create card template
     const cardElement = document.createElement("li");
     cardElement.classList.add("main__wrapper__cards__card");
-    cardElement.innerHTML = character.fullName;
+    let cardHtml = `
+    <h2 class="main__wrapper__cards__card__name">${character.fullName}</h2>
+    <span class="main__wrapper__cards__card__title">${character.title}</span>
+    <div class="main__wrapper__cards__card__img-container">
+      <img class="main__wrapper__cards__card__img-container__img" src=${character.imageUrl}>
+    </div>
+    `;
+    cardElement.innerHTML = cardHtml;
 
     fragment.appendChild(cardElement);
   })
